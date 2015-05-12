5eb0e090f203ccb7beecb29fdd16b06a8805b8b9
Andrew Mcveigh <me@andrewmcveigh.com>
2015-05-12 21:48:07 +0100

    Comment syntax-quote* and resolve symbol

--- src/main/clojurescript/clojure/tools/reader.cljs
+++ src/main/clojurescript/clojure/tools/reader.cljs
@@ -636,23 +636,25 @@
         (set! gensym-env (assoc gensym-env sym gs))
         gs)))
 
-(defn ^:dynamic resolve-symbol
-  "Resolve a symbol s into its fully qualified namespace version"
-  [s]
-  (if (pos? (.indexOf (name s) "."))
-    s ;; If there is a period, it is interop
-    (if-let [ns-str (namespace s)]
-      (let [ns (resolve-ns (symbol ns-str))]
-        (if (or (nil? ns)
-                (= (ns-name* ns) ns-str)) ;; not an alias
-          s
-          (symbol (ns-name* ns) (name s))))
-      (if-let [o ((ns-map *ns*) s)]
-        (if (class? o)
-          (symbol (.getName ^Class o))
-          (if (var? o)
-            (symbol (-> ^Var o .ns ns-name*) (-> ^Var o .sym name))))
-        (symbol (ns-name* *ns*) (name s))))))
+(comment
+  ;;; can't resolve symbol without ns availability in cljs
+  (defn ^:dynamic resolve-symbol
+   "Resolve a symbol s into its fully qualified namespace version"
+   [s]
+   (if (pos? (.indexOf (name s) "."))
+     s ;; If there is a period, it is interop
+     (if-let [ns-str (namespace s)]
+       (let [ns (resolve-ns (symbol ns-str))]
+         (if (or (nil? ns)
+                 (= (ns-name* ns) ns-str)) ;; not an alias
+           s
+           (symbol (ns-name* ns) (name s))))
+       (if-let [o ((ns-map *ns*) s)]
+         (if (class? o)
+           (symbol (.getName ^Class o))
+           (if (var? o)
+             (symbol (-> ^Var o .ns ns-name*) (-> ^Var o .sym name))))
+         (symbol (ns-name* *ns*) (name s)))))))
 
 (defn- add-meta [form ret]
   (if (and (instance? IWithMeta form)
@@ -684,61 +686,63 @@
       (true? x)
       (false? x)))
 
-(defn- syntax-quote* [form]
-  (->>
-   (cond
-    (special-symbol? form) (list 'quote form)
-
-    (symbol? form)
-    (list 'quote
-          (if (namespace form)
-            (let [maybe-class ((ns-map *ns*)
-                               (symbol (namespace form)))]
-              (if (class? maybe-class)
-                (symbol (.getName ^Class maybe-class) (name form))
-                (resolve-symbol form)))
-            (let [sym (name form)]
-              (cond
-               (.endsWith sym "#")
-               (register-gensym form)
-
-               (.startsWith sym ".")
-               form
-
-               (.endsWith sym ".")
-               (let [csym (symbol (subs sym 0 (dec (count sym))))]
-                 (symbol (.concat (name (resolve-symbol csym)) ".")))
-               :else (resolve-symbol form)))))
-
-    (unquote? form) (second form)
-    (unquote-splicing? form) (throw (IllegalStateException. "splice not in list"))
-
-    (coll? form)
+(comment
+  ;;; can't properly syntax-quote without resolve-symbol
+  (defn- syntax-quote* [form]
+   (->>
     (cond
+      (special-symbol? form) (list 'quote form)
 
-     (instance? IRecord form) form
-     (map? form) (syntax-quote-coll (map-func form) (flatten-map form))
-     (vector? form) (list 'clojure.core/vec (syntax-quote-coll nil form))
-     (set? form) (syntax-quote-coll 'clojure.core/hash-set form)
-     (or (seq? form) (list? form))
-     (let [seq (seq form)]
-       (if seq
-         (syntax-quote-coll nil seq)
-         '(clojure.core/list)))
+      (symbol? form)
+      (list 'quote
+            (if (namespace form)
+              (let [maybe-class ((ns-map *ns*)
+                                 (symbol (namespace form)))]
+                (if (class? maybe-class)
+                  (symbol (.getName ^Class maybe-class) (name form))
+                  (resolve-symbol form)))
+              (let [sym (name form)]
+                (cond
+                  (.endsWith sym "#")
+                  (register-gensym form)
 
-     :else (throw (UnsupportedOperationException. "Unknown Collection type")))
+                  (.startsWith sym ".")
+                  form
 
-    (or (keyword? form)
-        (number? form)
-        ;; (char? form) ;; no char type in cljs
-        (string? form)
-        (nil? form)
-        (bool? form)
-        (instance? js/RegExp form))
-    form
+                  (.endsWith sym ".")
+                  (let [csym (symbol (subs sym 0 (dec (count sym))))]
+                    (symbol (.concat (name (resolve-symbol csym)) ".")))
+                  :else (resolve-symbol form)))))
 
-    :else (list 'quote form))
-   (add-meta form)))
+      (unquote? form) (second form)
+      (unquote-splicing? form) (throw (IllegalStateException. "splice not in list"))
+
+      (coll? form)
+      (cond
+
+        (instance? IRecord form) form
+        (map? form) (syntax-quote-coll (map-func form) (flatten-map form))
+        (vector? form) (list 'clojure.core/vec (syntax-quote-coll nil form))
+        (set? form) (syntax-quote-coll 'clojure.core/hash-set form)
+        (or (seq? form) (list? form))
+        (let [seq (seq form)]
+          (if seq
+            (syntax-quote-coll nil seq)
+            '(clojure.core/list)))
+
+        :else (throw (UnsupportedOperationException. "Unknown Collection type")))
+
+      (or (keyword? form)
+          (number? form)
+          ;; (char? form) ;; no char type in cljs
+          (string? form)
+          (nil? form)
+          (bool? form)
+          (instance? js/RegExp form))
+      form
+
+      :else (list 'quote form))
+    (add-meta form))))
 
 (defn- read-syntax-quote
   [rdr backquote opts pending-forms]
